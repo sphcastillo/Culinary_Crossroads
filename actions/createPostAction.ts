7e62cf65bbe0efb8ad0933d56ba655993f585de4
sphcastillo <sphcastillo@gmail.com>
2024-06-20 17:46:03 -0700

    generateSASToken added

--- actions/createPostAction.ts
+++ actions/createPostAction.ts
@@ -37,7 +37,7 @@ export default async function createPostAction(formData: FormData){
           user: userDB,
           text: postInput,
         }
-        
+        await Post.create(body);
       } else {
         // 1) create post in database w/o image
 
